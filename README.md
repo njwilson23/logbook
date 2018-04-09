513893e5c0b4cdccfdd7323c82a89e6b78c529a5
Nat Wilson <natw@fortyninemaps.com>
2018-04-08 17:10:23 -0700

    add simple CLI search tool

--- README.md
+++ README.md
@@ -11,3 +11,13 @@ Opens an editor to a journal entry labelled with the current date
     LBDIR       default directory for journal files
     LBEDITOR    editor command
 
+## Server
+
+List log entries:
+
+    curl localhost:8080/list
+
+Search logs:
+
+    curl localhost:8080/list?find=INTERESTING_TEXT_SNIPPET
+
---
+++ logsearch/logsearch.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	protocol := "http"
+	server := "localhost:8080"
+
+	requestBuilder := new(bytes.Buffer)
+	requestBuilder.Write([]byte(protocol))
+	requestBuilder.Write([]byte("://"))
+	requestBuilder.Write([]byte(server))
+	requestBuilder.Write([]byte("/search?find="))
+	for _, arg := range args {
+		requestBuilder.Write([]byte(arg))
+	}
+
+	requestString := requestBuilder.String()
+
+	resp, err := http.Get(requestString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if resp.StatusCode != 200 {
+		log.Fatalf("Status: %s\n", resp.Status)
+	}
+
+	if resp.ContentLength > (1 << 20) {
+		log.Fatalf("response too large: %d", resp.ContentLength)
+	}
+
+	buffer := make([]byte, 1<<10)
+	for {
+		n, err := resp.Body.Read(buffer)
+		fmt.Print(string(buffer[:n]))
+		if err == io.EOF || n == 0 {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	fmt.Print("\n")
+
+	/*fmt.Println(resp.ContentLength)
+	body := make([]byte, resp.ContentLength)
+	n, err := resp.Body.Read(body)
+	if int64(n) != resp.ContentLength {
+		log.Println(err)
+	}
+
+	fmt.Println(string(body))*/
+}
