5e0d3bc2090b76ac04dc10884e5140cff4da348a
Nat Wilson <njwilson23@gmail.com>
2018-05-12 21:22:13 -0700

    write client tool in Go

---
+++ client/lb.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"time"
+)
+
+type Entry struct {
+	filename string
+	topic    string
+	date     time.Time
+	exists   bool
+}
+
+func (e *Entry) AddHeader() error {
+	if !e.exists {
+		f, err := os.Create(e.filename)
+		if err != nil {
+			fmt.Printf("failed to prepare %s", e.filename)
+			return err
+		}
+		defer f.Close()
+		f.Write([]byte(fmt.Sprintf("# %s\n", e.date.Format("2006 Jan 02"))))
+	}
+	return nil
+}
+
+func getEditor() string {
+	editor := os.Getenv("LBEDITOR")
+	if editor == "" {
+		editor = "vim"
+	}
+	return editor
+}
+
+func run(topic string, reldays int) {
+	entry := getEntry(topic, reldays)
+
+	err := entry.AddHeader()
+	if err != nil {
+		fmt.Printf("ERROR: %v\n", err)
+		return
+	}
+
+	cmd := exec.Command(getEditor(), entry.filename)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err = cmd.Run()
+	if err != nil {
+		fmt.Printf("ERROR: %v\n", err)
+	}
+}
+
+func getEntry(topic string, reldays int) *Entry {
+	notesDir := os.Getenv("LBDIR")
+	if notesDir == "" {
+		notesDir = filepath.Join(os.Getenv("HOME"), "notes")
+	}
+
+	date := time.Now().Truncate(time.Hour * 24).Add(time.Hour * time.Duration(reldays*24))
+	var topicStr string
+	if topic != "" {
+		topicStr = fmt.Sprintf("-%s", topic)
+	}
+	filename := filepath.Join(notesDir, fmt.Sprintf("%s%s.md", date.Format("2006-01-02"), topicStr))
+
+	exists := true
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		exists = false
+	}
+
+	return &Entry{
+		filename,
+		topic,
+		date,
+		exists,
+	}
+}
+
+func main() {
+	days := flag.Int("days", 0, "number of days relative to current date to open entry for")
+	topic := flag.String("topic", "", "entry topic")
+
+	flag.Parse()
+
+	run(*topic, *days)
+}
